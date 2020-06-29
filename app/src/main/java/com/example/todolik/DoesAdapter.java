c9c4a7b15c024e410bdc925dc83a45cc49f1b9ad
Vasilevskiy Pasha <vasilevskij00@inbox.ru>
2020-06-29 19:37:53 +0300

    features: implement doesadapter for recyclerview

---
+++ app/src/main/java/com/example/todolik/DoesAdapter.java
@@ -0,0 +1,54 @@
+package com.example.todolik;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class DataAdapter extends RecyclerView.Adapter {
+
+    Context _context;
+    ArrayList<Task> _tasks;
+
+    public DataAdapter(Context context, ArrayList<Task> tasks) {
+        _context = context;
+        _tasks = tasks;
+    }
+
+
+    // onCreateViewHolder: возвращает объект ViewHolder, который будет хранить данные по одному объекту Task.
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(_context).inflate(R.layout.task, parent,false));
+    }
+
+    //onBindViewHolder: выполняет привязку объекта ViewHolder к объекту Task по определенной позиции.
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.tasks.setText(_tasks.get(position).get_content());
+    }
+
+    //getItemCount: возвращает количество объектов в списке
+    @Override
+    public int getItemCount() {
+        return _tasks.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        CheckBox tasks;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tasks = itemView.findViewById(R.id.task);
+        }
+    }
+}
