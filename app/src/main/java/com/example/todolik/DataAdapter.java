4fc4074bb6399485f26519aa8f47a99be7618277
Vasilevskiy Pasha <vasilevskij00@inbox.ru>
2020-06-29 05:58:15 +0300

    features: simple addition

--- app/src/main/java/com/example/todolik/DataAdapter.java
+++ app/src/main/java/com/example/todolik/DataAdapter.java
@@ -1,12 +1,25 @@
 package com.example.todolik;
 
+import android.content.Context;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class DataAdapter extends RecyclerView.Adapter {
+
+    Context _context;
+    ArrayList<Task> _tasks;
+
+    public DataAdapter(Context context, ArrayList<Task> tasks) {
+        _context = context;
+        _tasks = tasks;
+    }
 
     @NonNull
     @Override
@@ -28,6 +41,7 @@ public class DataAdapter extends RecyclerView.Adapter {
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            task = (CheckBox)itemView.findViewById(R.id.task);
         }
     }
 }
